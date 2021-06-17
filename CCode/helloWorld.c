87243a282b34a9a7ac2ef1100d878a1c29e51571
sushil kumar <53127177+kaswanSushil@users.noreply.github.com>
2021-06-17 11:49:50 +0530

    Update helloWorld.c

--- CCode/helloWorld.c
+++ CCode/helloWorld.c
@@ -1,5 +1,12 @@
-#include<stdio.h>
-int main()
-{
-    printf("Hello world");
+#include <stdio.h>
+
+void greeting(char name[]);
+
+int main() {
+char name[] = "Alia Bhat"
+greeting(name);
 }
+
+void greeting(char name[]) {
+printf("Hey, Where was you?.. %s", name);
+} 
